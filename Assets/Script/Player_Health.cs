adcaeb1bad2e45abd02f97ab69b2c1e1fbd65c27
Harshanator <harshasharma1623@gmail.com>
2017-10-17 19:12:15 -0400

    Initial Commit

---
+++ Assets/Script/Player_Health.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Player_Health : MonoBehaviour {
+
+	
+	void Update () {
+
+		if (gameObject.transform.position.y < -6) {
+			Die ();
+		}
+			
+	}
+
+	void Die()
+	{
+		SceneManager.LoadScene ("Prototype_1");
+
+	}
+}
